71fd1b3e338d95aa1a9329210485d79a698ef5db
michzann <rafihamizan981@gmail.com>
2026-03-30 14:54:37 +0700

    re michie

--- grocerseeker/grocerseeker/grocerseeker/Form1.cs
+++ grocerseeker/grocerseeker/grocerseeker/Form1.cs
@@ -1,3 +1,4 @@
+using Guna.UI2.WinForms;
 using MySql.Data.MySqlClient;
 using System.Data;
 namespace grocerseeker
@@ -5,7 +6,7 @@ namespace grocerseeker
 {
     public partial class Form1 : Form
     {
-        DatabaseHelper dbHeloper = new DatabaseHelper();
+        DatabaseHelper dbHelper = new DatabaseHelper();
 
         public Form1()
         {
@@ -19,36 +20,56 @@ namespace grocerseeker
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string username = textBox1.Text;
-            string password = textBox2.Text;
+            string username = this.username.Text;
+            string pass = this.pass.Text;
 
-            using (MySqlConnection conn = dbHeloper.GetConnection())
+            using (MySqlConnection conn = dbHelper.GetConnection())
             {
                 try
                 {
+
+                    string query = "SELECT id, phone_number, cust_active, cust_name,vendor_name, password FROM users WHERE phone_number=@phone AND password=@pass";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@phone", username);
+                    cmd.Parameters.AddWithValue("@pass", pass);
+
                     conn.Open();
-                    string query = "SELECT role FROM users WHERE username = @username AND password = @password";
-                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
                     {
-                        cmd.Parameters.AddWithValue("@username", username);
-                        cmd.Parameters.AddWithValue("@password", password);
+                        UserSession.UserID = reader["id"].ToString();
+                        UserSession.PhoneNumber = reader["phone_number"].ToString();
 
-                        object result = cmd.ExecuteScalar();
+                        int isCust = Convert.ToInt32(reader["cust_active"]);
+                        string selectedRole = role.SelectedItem.ToString();
 
-                        if (result != null)
+
+                        if (selectedRole == "Customer" && isCust == 1)
                         {
-                            string role = result.ToString();
-                            MessageBox.Show("Login successful! Role: " + role);
-                            MainForm Form1 = new MainForm();
-                            Form1.Show();
-                            this.Hide();
+                            UserSession.UserRole = "Customer";
+                            UserSession.Username = reader["cust_name"].ToString();
+                        }
+                        else if (selectedRole == "Vendor" && isCust == 0)
+                        {
+                            UserSession.UserRole = "Vendor";
+                            UserSession.Username = reader["vendor_name"].ToString();
                         }
                         else
                         {
-                            MessageBox.Show("Invalid username or password.");
+                            MessageBox.Show("Role tidak sesuai dengan akun!", "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
                         }
+
+                        MessageBox.Show("Login Berhasil!", "Sukses");
+
+                        this.Hide();
+                        MainForm Form1 = new MainForm();
+                        Form1.ShowDialog();
                     }
                 }
+
+
                 catch (Exception ex)
                 {
                     MessageBox.Show("Error: " + ex.Message);
@@ -61,6 +82,16 @@ namespace grocerseeker
             this.Hide();
             Register Form1 = new Register();
             Form1.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- grocerseeker/grocerseeker/grocerseeker/MainForm.Designer.cs
+++ grocerseeker/grocerseeker/grocerseeker/MainForm.Designer.cs
@@ -28,12 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            Login = new Label();
+            label1 = new Label();
+            Profile = new Guna.UI2.WinForms.Guna2Button();
+            Product = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button6 = new Guna.UI2.WinForms.Guna2Button();
+            LogOut = new Guna.UI2.WinForms.Guna2Button();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2GradientPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2GradientPanel1
+            // 
+            guna2GradientPanel1.BackColor = Color.Transparent;
+            guna2GradientPanel1.BorderColor = Color.Transparent;
+            guna2GradientPanel1.Controls.Add(LogOut);
+            guna2GradientPanel1.Controls.Add(guna2Button6);
+            guna2GradientPanel1.Controls.Add(Product);
+            guna2GradientPanel1.Controls.Add(Profile);
+            guna2GradientPanel1.Controls.Add(Login);
+            guna2GradientPanel1.Controls.Add(label1);
+            guna2GradientPanel1.CustomBorderColor = Color.FromArgb(255, 224, 192);
+            guna2GradientPanel1.CustomizableEdges = customizableEdges9;
+            guna2GradientPanel1.FillColor = Color.FromArgb(128, 255, 255);
+            guna2GradientPanel1.FillColor2 = Color.FromArgb(192, 255, 255);
+            guna2GradientPanel1.Location = new Point(-3, -1);
+            guna2GradientPanel1.Name = "guna2GradientPanel1";
+            guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2GradientPanel1.Size = new Size(183, 452);
+            guna2GradientPanel1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            Login.AutoSize = true;
+            Login.Location = new Point(15, 68);
+            Login.Name = "Login";
+            Login.Size = new Size(43, 15);
+            Login.TabIndex = 2;
+            Login.Text = "Login, ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Welcome, ";
+            // 
+            // Profile
+            // 
+            Profile.BackColor = Color.Transparent;
+            Profile.BorderColor = Color.Transparent;
+            Profile.BorderRadius = 15;
+            Profile.CustomizableEdges = customizableEdges7;
+            Profile.DisabledState.BorderColor = Color.DarkGray;
+            Profile.DisabledState.CustomBorderColor = Color.DarkGray;
+            Profile.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Profile.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Profile.FillColor = Color.Turquoise;
+            Profile.Font = new Font("Segoe UI", 9F);
+            Profile.ForeColor = Color.White;
+            Profile.Location = new Point(28, 177);
+            Profile.Name = "Profile";
+            Profile.PressedDepth = 25;
+            Profile.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            Profile.Size = new Size(129, 38);
+            Profile.TabIndex = 3;
+            Profile.Text = "Profile";
+            Profile.Click += guna2Button1_Click;
+            // 
+            // Product
+            // 
+            Product.BackColor = Color.Transparent;
+            Product.BorderColor = Color.Transparent;
+            Product.BorderRadius = 15;
+            Product.CustomizableEdges = customizableEdges5;
+            Product.DisabledState.BorderColor = Color.DarkGray;
+            Product.DisabledState.CustomBorderColor = Color.DarkGray;
+            Product.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Product.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Product.FillColor = Color.Turquoise;
+            Product.Font = new Font("Segoe UI", 9F);
+            Product.ForeColor = Color.White;
+            Product.Location = new Point(28, 221);
+            Product.Name = "Product";
+            Product.PressedDepth = 25;
+            Product.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            Product.Size = new Size(129, 38);
+            Product.TabIndex = 7;
+            Product.Text = "Product";
+            Product.Click += guna2Button5_Click;
+            // 
+            // guna2Button6
+            // 
+            guna2Button6.BackColor = Color.Transparent;
+            guna2Button6.BorderColor = Color.Transparent;
+            guna2Button6.BorderRadius = 15;
+            guna2Button6.CustomizableEdges = customizableEdges3;
+            guna2Button6.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button6.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button6.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button6.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button6.FillColor = Color.Turquoise;
+            guna2Button6.Font = new Font("Segoe UI", 9F);
+            guna2Button6.ForeColor = Color.White;
+            guna2Button6.Location = new Point(28, 265);
+            guna2Button6.Name = "guna2Button6";
+            guna2Button6.PressedDepth = 25;
+            guna2Button6.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Button6.Size = new Size(129, 38);
+            guna2Button6.TabIndex = 8;
+            guna2Button6.Text = "Transaction";
+            guna2Button6.Click += guna2Button6_Click;
+            // 
+            // LogOut
+            // 
+            LogOut.BackColor = Color.Transparent;
+            LogOut.BorderColor = Color.Transparent;
+            LogOut.BorderRadius = 15;
+            LogOut.CustomizableEdges = customizableEdges1;
+            LogOut.DisabledState.BorderColor = Color.DarkGray;
+            LogOut.DisabledState.CustomBorderColor = Color.DarkGray;
+            LogOut.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            LogOut.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            LogOut.FillColor = Color.Turquoise;
+            LogOut.Font = new Font("Segoe UI", 9F);
+            LogOut.ForeColor = Color.White;
+            LogOut.Location = new Point(28, 382);
+            LogOut.Name = "LogOut";
+            LogOut.PressedDepth = 25;
+            LogOut.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            LogOut.Size = new Size(129, 38);
+            LogOut.TabIndex = 9;
+            LogOut.Text = "LogOut";
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.CustomizableEdges = customizableEdges11;
+            guna2Panel1.Location = new Point(177, -1);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2Panel1.Size = new Size(623, 452);
+            guna2Panel1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(guna2Panel1);
+            Controls.Add(guna2GradientPanel1);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            guna2GradientPanel1.ResumeLayout(false);
+            guna2GradientPanel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Label Login;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2Button Profile;
+        private Guna.UI2.WinForms.Guna2Button LogOut;
+        private Guna.UI2.WinForms.Guna2Button guna2Button6;
+        private Guna.UI2.WinForms.Guna2Button Product;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
     }
 }
